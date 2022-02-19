61f6fab22404a402b326898a12cf37c8dfe56a76
Vinicius Rocha <vinirocha05@gmail.com>
2022-02-19 18:46:14 -0300

    alguns ajustes

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import { FaBars } from 'react-icons/fa'
 
 export default function Header(props) {
-    const [click, setClick] = useState(false);
+    const [click, setClick] = useState(true);
     function handleClick() {
         setClick(!click)
     }
@@ -18,24 +18,24 @@ export default function Header(props) {
                 <div>
                     <nav className="flex-1">
                         <ul className="text-white hidden md:flex ">
-                            <li className="mx-2"><Link href="">Sobre</Link></li>
-                            <li className="mx-2"><Link href="">Produtos</Link></li>
-                            <li className="mx-2"><Link href="">Contato</Link></li>
+                            <li className="mx-2"><Link passHref  href="/">Sobre</Link></li>
+                            <li className="mx-2"><Link passHref href="/">Produtos</Link></li>
+                            <li className="mx-2"><Link passHref href="/">Contato</Link></li>
                         </ul>
                     </nav>
                 </div>
             </div>
-            <div className={`h-screen text-blue-100 z-10 w-64 absolute top-0 bg-blue-700 transform transition duration-200 ease-in-out ${click? '-translate-x-full':''}`}>
-                <div className="flex justify-center items-center "><Link href="/" passHref><Image src='/assets/img/dentistry.png' alt="img" width='60' height='60' /></Link>
+            <div className={`h-screen text-blue-100 z-10 w-64 absolute top-0 bg-blue-700 transform transition duration-500 ease-in-out ${click? '-translate-x-full':''}`}>
+                <div className="flex justify-center items-center py-6 "><Link href="/" passHref><Image src='/assets/img/dentistry.png' alt="img" width='60' height='60' /></Link>
                     <span className="ml-2">Clínicas sempre sorrindo</span></div>
                 <nav className="flex-1">
                     <ul className="text-white text-xl mt-5">
-                        <li className="py-5 ml-8"><Link href="">Sobre</Link></li>
-                        <li className="py-5 ml-8"><Link href="">Produtos</Link></li>
-                        <li className="py-5 ml-8"><Link href="">Contato</Link></li>
+                        <li className="py-5 ml-8"><Link passHref href="/">Sobre</Link></li>
+                        <li className="py-5 ml-8"><Link passHref href="/">Produtos</Link></li>
+                        <li className="py-5 ml-8"><Link passHref href="/">Contato</Link></li>
                     </ul>
                 </nav>
             </div>
         </header>
     )
-};
+}; 
