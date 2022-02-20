20caca06683fb7c963339a76a47e3cf08c83f873
Vinicius Rocha <vinirocha05@gmail.com>
2022-02-19 23:37:40 -0300

    teste sem header lateral no mobile

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -18,14 +18,24 @@ export default function Header(props) {
                 <div>
                     <nav className="flex-1">
                         <ul className="text-white hidden md:flex ">
-                            <li className="mx-2"><Link passHref  href="/">Sobre</Link></li>
+                            <li className="mx-2"><Link passHref href="/">Sobre</Link></li>
                             <li className="mx-2"><Link passHref href="/">Produtos</Link></li>
                             <li className="mx-2"><Link passHref href="/">Contato</Link></li>
                         </ul>
                     </nav>
                 </div>
             </div>
-
+            <div className={`h-screen text-blue-100 z-10 w-64 absolute top-0 md:hidden bg-blue-700 transform transition duration-500 ease-in-out ${click ? '-translate-x-full' : ''}`}>
+                <div className="flex justify-center items-center py-6 "><Link href="/" passHref><Image src='/assets/img/dentistry.webp' alt="img" width='60' height='60' /></Link>
+                    <span className="ml-2">Clínicas sempre sorrindo</span></div>
+                <nav className="flex-1">
+                    <ul className="text-white text-xl mt-5">
+                        <li className="py-5 ml-8"><Link passHref href="/">Sobre</Link></li>
+                        <li className="py-5 ml-8"><Link passHref href="/">Produtos</Link></li>
+                        <li className="py-5 ml-8"><Link passHref href="/">Contato</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
 }; 
